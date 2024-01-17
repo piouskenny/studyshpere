3a21b71abee08d008afa676070d3eca2cb1e63d4
piouskenny <kehindeu13@gmail.com>
2024-01-17 17:07:38 +0100

    added courses contents migration

---
+++ database/migrations/2024_01_16_154725_create_course_contents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('courses')->cascadeOnDelete();
+            $table->enum('content_type', ['video', 'document']);
+            $table->string('content_url');
+            $table->unsignedInteger('order')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_contents');
+    }
+};
