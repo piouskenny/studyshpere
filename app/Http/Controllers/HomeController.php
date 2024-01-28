dce0f68f8fdfb0a52f74b37b07bd88bd03540092
piouskenny <kehindeu13@gmail.com>
2024-01-28 21:51:13 +0100

    teseting

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 
     public function courses()
     {
-        $courses = Courses::paginate(8);
+        $courses = Courses::paginate(6);
 
         return view(
             'courses',
