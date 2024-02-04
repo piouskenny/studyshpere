4c6925e70689415bc0c8eee885e5693cbde8e8d5
piouskenny <kehindeu13@gmail.com>
2024-02-04 16:42:55 +0100

    tutor can now upload pdf

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -13,13 +13,20 @@ class CourseController extends Controller
         $request->validate([
             'course_id' => 'required|integer',
             'content_type' => 'required|string',
-            'content_url' => 'required|string',
+            'content_url' => 'string',
+            'course_file' =>  'mimes:pdf,ppt'
         ]);
+
+
+
+
+        $course_file_name = time() . '-' . $request->course_id . '.' . $request->course_file->extension();
+        $request->course_file->move(public_path('course_file'), $course_file_name);
 
         CourseContent::create([
             'courses_id' => $request->course_id,
             'content_type' => $request->content_type,
-            'content_url' => $request->content_url
+            'content_url' => $request->content_url ?? $course_file_name
         ]);
 
         return redirect(route('tutorSingleCourse', $request->course_id))->with('');
--- resources/views/Tutor/createCourseContent.blade.php
+++ resources/views/Tutor/createCourseContent.blade.php
@@ -7,7 +7,7 @@
                 <h3 class="text-center text-primary">
                     Add Course Content
                 </h3>
-                <form action="{{route('createCourseContentPost')}}" method="post">
+                <form action="{{route('createCourseContentPost')}}" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('POST')
                     <input type="text" name="course_id" value="{{$course->id}}" class="form-control" hidden />
@@ -19,15 +19,30 @@
                         <select class="form-control" name="content_type">
                             <option value="">Select Content Type</option>
                             <option value="video">Video</option>
-                            <option value="document">Document</option>
+                            <option value="document">Pdf or ppt</option>
                         </select>
                     </div>
+
+                    @if ($course->course_type == "pdf")
+                    <div data-mdb-input-init class="form-outline mb-4">
+                        <label class="form-label" for="form6Example5">Course File</label>
+                        <input type="file" id="form6Example5" class="form-control-file" name="course_file" />
+                        <span class="d-block text-danger">
+                            @error('course_file')
+                            {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+
+                    @else
+
                     <div class="form-group shadow-sm">
                         <label for="content_url">
                             content Url
                         </label>
                         <input type="text" name="content_url" class="form-control" />
                     </div>
+                    @endif
                     <div class="form-group d-flex justify-content-center">
                         <button class="btn btn-primary w-75">Add Content</button>
                     </div>
